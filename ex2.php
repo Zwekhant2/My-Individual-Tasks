707d4ed80814e4b5bc887202f556db5884b480bd
zwekhantlwin <zwekhantlwin5@gmail.com>
2024-01-29 11:38:54 +0200

    asdf

---
+++ ex2.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+<div class="row">
+
+            <div class="col-sm-7">
+    <h1>Bootstrap Example</h1>
+    <p>A wiki (/ˈwɪki/ ⓘ WI-kee) is a form of online hypertext publication, collaboratively edited and managed by its own audience, using a web browser. A typical wiki contains multiple pages for the subjects or scope of the project, and could be either open to the public or limited to use within an organization for maintaining its internal knowledge base.</p>
+    <p>There are hundreds of thousands of wikis in use, both public and private, including wikis functioning as knowledge management resources, note-taking tools, community websites, and intranets. Ward Cunningham, the developer of the first wiki software, WikiWikiWeb, originally described wiki as "the simplest online database that could possibly work".[3] "Wiki" (pronounced [wiki][note 1]) is a Hawaiian word meaning "quick".</p>
+    <a href="#" class="btn btn-danger">Contact Us</a>  
+            </div>   
+
+            <div class="col-sm-5">
+    <img class="img-fluid" src="images/logan paul.jpg">
+            </div>
+
+        </div>
+<?php include 'footer.php';?>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="row">
+            <div class="col-md-4">coloum 1</div>
+            <div class="col-md-4">coloum 2</div>
+            <div class="col-md-4">coloum 3</div>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
